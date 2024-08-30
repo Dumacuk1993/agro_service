cd4de079601785ddd23b0f5f67376c2cae63caf3
Dmitry <dumakuryndin@yandex.ru>
2024-08-30 17:22:16 +0300

    доработка по странице

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ window.addEventListener('DOMContentLoaded', () => {
     $('.slider_main').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
         infinite: true,
         speed: 500,
         fade: true,
-        arrows: false,
+        arrows: true,
+        dots: true
     });
     
     
