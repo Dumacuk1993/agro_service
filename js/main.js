b1d2adf0065417478636111a5607707456399350
Dmitry Kuryndin <dimakuryndin1@yandex.ru>
2024-09-25 15:24:33 +0300

    Доделана страница продукта

--- js/main.js
+++ js/main.js
@@ -42,6 +42,56 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
     })
-    
 
+
+
+    const buttons = document.querySelectorAll('.tabs_header_item');
+    const contents = document.querySelectorAll('.tabs_content_item');
+
+    function showTab(tabId) {
+        buttons.forEach(btn => btn.classList.remove('tabs_header_item--active'));
+        contents.forEach(content => (content.style.display = 'none'));
+        document.querySelector(`.tabs_header_item[data-tab="${tabId}"]`).classList.add('tabs_header_item--active');
+        document.getElementById(tabId).style.display = 'block';
+    }
+
+    showTab('tab-2');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+        const tabId = button.getAttribute('data-tab');
+        showTab(tabId);
+        });
+    });
+    
+    const productThumbs = document.querySelectorAll('.product_img-line--img'),
+          productCurrent = document.querySelector('.product_img-main--img img');
+
+    productThumbs.forEach(thumb => {
+        thumb.addEventListener('click', function() {
+            const thumbImg = this.querySelector('img');
+            productCurrent.setAttribute('src', thumbImg.getAttribute('src'));   
+        })
+    })
+
+    const faqEls = document.querySelectorAll('.product_right-advantage-toggle-title');
+
+    faqEls.forEach(faq => {
+        faq.addEventListener('click', function() {
+            const answer = this.nextElementSibling;
+            const iconElement = this.querySelector('i');
+
+            if (answer.classList.contains('open')) {
+                answer.classList.remove('open');
+                answer.style.maxHeight = null;
+                answer.style.padding = '0px 16px';
+                iconElement.style.transform = 'rotate(-90deg)';
+            } else {
+                answer.classList.add('open');
+                answer.style.maxHeight = answer.scrollHeight + 32 + 'px';
+                answer.style.padding = '16px';
+                iconElement.style.transform = 'rotate(0deg)';
+            }
+        });
+    });
 })
